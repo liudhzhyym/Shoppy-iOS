48feed0401741e6db9b09e4d193c1b486a622ab4
Victor Lourme <victor.lourme@icloud.com>
2020-07-06 13:45:05 +0200

    Views/Settings: added credits and issue reporting

--- Shoppy/Views/Settings/SettingsView.swift
+++ Shoppy/Views/Settings/SettingsView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 
 struct SettingsView: View {
     @State private var displayLogin = false
+    @State private var credits = [
+        "SwiftyShoppy": "https://github.com/vlourme/SwiftyShoppy",
+        "Moya": "https://github.com/Moya/Moya",
+        "KeychainSwift": "https://github.com/evgenyneu/keychain-swift"
+    ]
     
     // Change key
     private func changeKey() {
@@ -24,6 +29,44 @@ struct SettingsView: View {
                         HStack(spacing: 10) {
                             Image(systemName: "lock.fill")
                             Text("Change API key")
+                        }
+                    }
+                }
+                
+                Section(header: Text("Source code")) {
+                    Button(action: {
+                        if let url = URL(string: "https://github.com/vlourme/Shoppy-iOS/issues") {
+                            UIApplication.shared.open(url)
+                        }
+                    }) {
+                        HStack(spacing: 10) {
+                            Image(systemName: "exclamationmark.bubble.fill")
+                            Text("Report an issue")
+                        }
+                    }
+                    
+                    Button(action: {
+                        if let url = URL(string: "https://github.com/vlourme/Shoppy-iOS") {
+                            UIApplication.shared.open(url)
+                        }
+                    }) {
+                        HStack(spacing: 10) {
+                            Image(systemName: "doc.text.fill")
+                            Text("View the source code on GitHub")
+                        }
+                    }
+                }
+                
+                Section(header: Text("Libraries used")) {
+                    List {
+                        ForEach(credits.sorted(by: >), id: \.key) { name, url in
+                            Button(action: {
+                                if let url = URL(string: url) {
+                                    UIApplication.shared.open(url)
+                                }
+                            }) {
+                                Text(name)
+                            }
                         }
                     }
                 }
