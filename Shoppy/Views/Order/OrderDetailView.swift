ec7bc0cad6c6d4ab40cf218ade027d1f8b2a4d0f
Victor Lourme <victor.lourme@icloud.com>
2020-07-06 19:55:33 +0200

    Views/OrderDetail: added more details

--- Shoppy/Views/Order/OrderDetailView.swift
+++ Shoppy/Views/Order/OrderDetailView.swift
@@ -18,17 +18,22 @@ struct OrderDetailView: View {
                 Section {
                     Field(key: "Order ID", value: order.id ?? "Unknown")
                     Field(key: "Product ID", value: order.product_id ?? "Unknown")
-                    Field(key: "Price", value: "\(order.price ?? 0)\(Currencies.getSymbol(forCurrencyCode: order.currency ?? "USD") ?? "USD")")
-                    Field(key: "Quantity", value: "\(order.quantity ?? 0)")
-                    Field(key: "Total", value: "\((order.price ?? 0) * Double(order.quantity ?? 0))\(Currencies.getSymbol(forCurrencyCode: order.currency ?? "USD") ?? "USD")")
                     Field(key: "Currency", value: order.currency ?? "USD")
+                    Field(key: "Date", value: order.created_at?.description ?? "Unknown")
+                    
                     if order.delivered == 1 {
                         Field(key: "Status", value: "Paid")
+                        Field(key: "Paid using", value: order.gateway ?? "Unknown")
+                        Field(key: "Paid at", value: order.paid_at?.description ?? "Unknown")
                     } else {
                         Field(key: "Status", value: "Not paid")
                     }
-                    
-                    Field(key: "Gateway", value: order.gateway ?? "Unknown")
+                }
+                
+                Section(header: Text("Payment")) {
+                    Field(key: "Price", value: "\(order.price ?? 0)\(Currencies.getSymbol(forCurrencyCode: order.currency ?? "USD") ?? "USD")")
+                    Field(key: "Quantity", value: "\(order.quantity ?? 0)")
+                    Field(key: "Total", value: "\((order.price ?? 0) * Double(order.quantity ?? 0))\(Currencies.getSymbol(forCurrencyCode: order.currency ?? "USD") ?? "USD")")
                 }
                 
                 Section(header: Text("Buyer")) {
@@ -44,6 +49,18 @@ struct OrderDetailView: View {
                         NavigationLink(destination: ProductDetailView(product: order.product!)) {
                             Text("See the product page")
                         }
+                    }
+                }
+                
+                Section(header: Text("Additional information")) {
+                    Field(key: "Hash", value: order.hash ?? "Unknown")
+                    
+                    if order.is_replacement ?? false {
+                        Field(key: "Replacement ID", value: order.replacement_id ?? "Unknown")
+                    }
+                    
+                    if order.coupon_id != nil {
+                        Field(key: "Coupon ID", value: order.coupon_id ?? "Unknown")
                     }
                 }
             }
