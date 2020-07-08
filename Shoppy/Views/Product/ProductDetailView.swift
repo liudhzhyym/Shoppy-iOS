dbf2e977228889e98393e12fd1c4ed20451fb19f
Victor Lourme <victor.lourme@icloud.com>
2020-07-08 11:59:33 +0200

    Views/Product: ability to delete a product
    * And fixed when product is not an account

--- Shoppy/Views/Product/ProductDetailView.swift
+++ Shoppy/Views/Product/ProductDetailView.swift
@@ -11,27 +11,71 @@ import SwiftyShoppy
 import KeychainSwift
 
 struct ProductDetailView: View {
+    @Environment(\.presentationMode) var presentation
     @State public var product: Product
-    @State private var isUnlisted: Bool = false
+    @State private var isPresented = false
+    private let keychain = KeychainSwift()
     
+    // Delete product
+    private func deleteProduct() {
+        // Get product ID
+        guard let id = product.id else {
+            return
+        }
+        
+        // Get token
+        if let key = keychain.get("key") {
+            NetworkManager
+                .prepare(token: key)
+                .target(.deleteProduct(id))
+                .asObject(UpdatedProduct.self, success: { update in
+                    // Dismiss
+                    self.presentation.wrappedValue.dismiss()
+                }, error: { error in
+                    print(error)
+                })
+        }
+    }
+    
+    // Open action sheet
+    var showAction: some View {
+        Button(action: {
+            self.isPresented = true
+        }) {
+            Image(systemName: "square.and.pencil")
+                .resizable()
+                .frame(width: 26, height: 26)
+        }
+    }
+    
+    // Body
     var body: some View {
         ScrollView {
             Container {
-                ContainerNavigationButton(title: "See the description", icon: "text.alignleft", destination: AnyView(ProductDetailledView(name: "Description", value: self.product.description!)))
+                if self.product.description != nil {
+                    ContainerNavigationButton(title: "See the description", icon: "text.alignleft", destination: AnyView(ProductDetailledView(name: "Description", value: self.product.description ?? "")))
+                }
                 ContainerField(name: "Delivery type", value: self.product.type?.rawValue.capitalized ?? "", icon: "cube.box")
             }
             
             Container {
                 ContainerField(name: "Price", value: "\(self.product.price ?? 0)", icon: "bag.fill")
-                ContainerField(name: "Revenue per order", value: "\((self.product.price ?? 0) * Double(self.product.quantity?.min ?? 0))", icon: "equal.square")
-                ContainerField(name: "Potential total", value: "\((self.product.price ?? 0) * Double(self.product.stock?.get() ?? 0))", icon: "equal.square.fill")
+                if self.product.type == .account {
+                    ContainerField(name: "Revenue per order", value: "\((self.product.price ?? 0) * Double(self.product.quantity?.min ?? 0))", icon: "equal.square")
+                    ContainerField(name: "Potential total", value: "\((self.product.price ?? 0) * Double(self.product.stock?.get() ?? 0))", icon: "equal.square.fill")
+                }
                 ContainerField(name: "Currency", value: self.product.currency ?? "", icon: "dollarsign.circle.fill")
             }
             
-            Container {
-                ContainerField(name: "Stock", value: "\(self.product.stock?.get() ?? 0)", icon: "cart.fill")
-                ContainerField(name: "Minimum quantity per order", value: "\(self.product.quantity?.min ?? 0)", icon: "minus.circle.fill")
-                ContainerField(name: "Maximum quantity per order", value: "\(self.product.quantity?.max ?? 0)", icon: "plus.circle.fill")
+            if self.product.type == .account || self.product.type == .dynamic {
+                Container {
+                    if self.product.type == .account {
+                        ContainerField(name: "Stock", value: "\(self.product.stock?.get() ?? 0)", icon: "cart.fill")
+                    }
+                    
+                    ContainerField(name: "Minimum quantity per order", value: "\(self.product.quantity?.min ?? 0)", icon: "minus.circle.fill")
+                    ContainerField(name: "Maximum quantity per order", value: "\(self.product.quantity?.max ?? 0)", icon: "plus.circle.fill")
+                }
             }
             
             Container {
@@ -44,8 +88,12 @@ struct ProductDetailView: View {
             Spacer()
         }
         .navigationBarTitle(product.title ?? "Product")
-        .onAppear {
-            self.isUnlisted = self.product.unlisted ?? false
+        .navigationBarItems(trailing: showAction)
+        .actionSheet(isPresented: $isPresented) {
+            ActionSheet(title: Text("Select an action"), buttons: [
+                .destructive(Text("Delete product"), action: self.deleteProduct),
+                .cancel()
+            ])
         }
     }
 }
