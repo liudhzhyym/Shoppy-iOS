838566475ab8c843f5fb36a8cc4f78598fe3f2d1
Victor Lourme <victor.lourme@icloud.com>
2020-07-22 19:44:41 +0200

    Views/QueriesView: Drop StackNavigationStyle

--- Shoppy/Views/Query/QueriesView.swift
+++ Shoppy/Views/Query/QueriesView.swift
@@ -57,7 +57,7 @@ struct QueriesView: View {
             .listStyle(GroupedListStyle())
             .navigationBarTitle("Support center")
             .navigationBarItems(trailing: refreshButton)
-        }.navigationViewStyle(StackNavigationViewStyle())
+        }
     }
 }
 
