6361533bf69ac2c22bd0aa70b8a2ae2a32e60e50
Victor Lourme <victor.lourme@icloud.com>
2020-08-15 11:09:52 +0200

    Views/User: light-mode fixes

--- Shoppy/Views/User/UserView.swift
+++ Shoppy/Views/User/UserView.swift
@@ -12,7 +12,7 @@ import KingfisherSwiftUI
 
 struct UserView: View {
     @State public var network: NetworkObserver
-
+    
     var profileImage: some View {
         Group {
             if network.settings?.settings?.userAvatarURL != nil {
@@ -23,81 +23,73 @@ struct UserView: View {
                     .resizable()
             }
         }
-        .frame(width: 60, height: 60)
-        .clipShape(Circle())
+        .frame(width: 65, height: 65)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
     }
-
+    
     var body: some View {
         NavigationView {
-            VStack {
+            List {
                 HStack {
                     profileImage
                         .padding(.horizontal)
-
+                    
                     VStack(alignment: .leading) {
                         Text(network.settings?.user?.username ?? "Username")
                             .font(.largeTitle)
                             .bold()
-
+                        
                         Text(network.settings?.user?.email ?? "email@domain.tld")
                             .font(.headline)
                     }
                     .lineLimit(0)
-
+                    
                     Spacer()
                 }
                 .padding()
-                .padding(.top)
-
-                List {
-                    Section(header: Text("Account")) {
-                        Label(label: "Currency",
-                              value: self.network.settings?.settings?.currency ?? "USD",
-                              icon: "dollarsign.circle.fill")
-                    }
-
-                    Section(header: Text("Payments")) {
-                        Label(label: "BTC Address",
-                              value: self.network.settings?.settings?.bitcoinAddress ?? "N/A",
-                              icon: "b.circle.fill",
-                              color: .orange)
-
-                        Label(label: "LTC Address",
-                              value: self.network.settings?.settings?.litecoinAddress ?? "N/A",
-                              icon: "l.circle.fill",
-                              color: .orange)
-
-                        Label(label: "ETH Address",
-                              value: self.network.settings?.settings?.ethereumAddress ?? "N/A",
-                              icon: "e.circle.fill",
-                              color: .orange)
-
-                        Label(label: "PayPal",
-                              value: self.network.settings?.settings?.paypalAddress ?? "N/A",
-                              icon: "p.circle.fill",
-                              color: .orange)
-
-                        Label(label: "Stripe ID",
-                              value: self.network.settings?.settings?.stripeAccountId ?? "N/A",
-                              icon: "s.circle.fill",
-                              color: .orange)
-                    }
-
-                    Section(header: Text("Customers")) {
-                        NavigationLink(destination: FeedbackView(network: self.network)) {
-                            Label(label: "See feedbacks", icon: "hand.thumbsup.fill", color: .green)
-                        }
+                
+                Section(header: Text("Account")) {
+                    Label(label: "Currency",
+                          value: self.network.settings?.settings?.currency ?? "USD",
+                          icon: "dollarsign.circle.fill")
+                }
+                
+                Section(header: Text("Payments")) {
+                    Label(label: "BTC Address",
+                          value: self.network.settings?.settings?.bitcoinAddress ?? "N/A",
+                          icon: "b.circle.fill",
+                          color: .orange)
+                    
+                    Label(label: "LTC Address",
+                          value: self.network.settings?.settings?.litecoinAddress ?? "N/A",
+                          icon: "l.circle.fill",
+                          color: .orange)
+                    
+                    Label(label: "ETH Address",
+                          value: self.network.settings?.settings?.ethereumAddress ?? "N/A",
+                          icon: "e.circle.fill",
+                          color: .orange)
+                    
+                    Label(label: "PayPal",
+                          value: self.network.settings?.settings?.paypalAddress ?? "N/A",
+                          icon: "p.circle.fill",
+                          color: .orange)
+                    
+                    Label(label: "Stripe ID",
+                          value: self.network.settings?.settings?.stripeAccountId ?? "N/A",
+                          icon: "s.circle.fill",
+                          color: .orange)
+                }
+                
+                Section(header: Text("Customers"),
+                        footer: Text("Your profile can be changed on the website.")) {
+                    NavigationLink(destination: FeedbackView(network: self.network)) {
+                        Label(label: "See feedbacks", icon: "hand.thumbsup.fill", color: .green)
                     }
                 }
-                .listStyle(GroupedListStyle())
-
-                Text("Your profile can be changed on the website.")
-                    .font(.footnote)
-                    .foregroundColor(.secondary)
-
-                Spacer()
             }
-
+            .listStyle(GroupedListStyle())
+            
             .navigationBarTitle("Profile", displayMode: .inline)
             .navigationBarItems(trailing: NavigationLink(destination: SettingsView(network: self.network)) {
                 Image(systemName: "gear")
